00c335833263dc4b4e91e6555d157646da12072e
Zem860 <zemmyhammy@gmail.com>
2025-03-15 16:04:43 +0800

    feat: add a placeholder for post test a new recipe page

---
+++ RecipeTest/Pages/RecipePlaceholder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeTest.Pages
+{
+    public class RecipePlaceholder
+    {
+        public string RecipeName { get; set; }
+        public string CoverPhoto { get; set; }
+    }
+}

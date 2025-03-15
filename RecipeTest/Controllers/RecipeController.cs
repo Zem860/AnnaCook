63b25718b12022843713ed73e09dff3b66f1b6ac
Zem860 <zemmyhammy@gmail.com>
2025-03-15 14:53:50 +0800

    feat: add cardType

---
+++ RecipeTest/Controllers/RecipeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RecipeTest.Models;
+using RecipeTest.Pages;
+using static RecipeTest.Controllers.HomeController;
+
+namespace RecipeTest.Controllers
+{
+    public class RecipeController : ApiController
+    {
+        private Model1 db = new Model1();
+        [HttpGet]
+        [Route("api/recipes")]
+        public IHttpActionResult getAllRecipe()
+        {
+            var recipesWithPhotos = db.Recipe
+             .GroupJoin(
+                 db.RecipePhotos,
+                 recipe => recipe.Id,         // 連接主鍵 (Recipe.Id)
+                 photo => photo.RecipeId,     // 連接外鍵 (RecipePhotos.RecipeId)
+                 (recipe, photos) => new RecipeCard
+                 {
+                     RecipeName = recipe.RecipeName,
+                     CoverPhoto = photos.FirstOrDefault(p => p.IsCover == true).ImgUrl, // ✅ 正確過濾封面圖片
+                     RecipeIntro = recipe.RecipeIntro,
+                     CookingTime = recipe.CookingTime.ToString(),
+                     Portion = recipe.Portion,
+                     Rating = recipe.Rating,
+                 });
+            return Ok(recipesWithPhotos);
+        }
+
+    }
+}
---
+++ RecipeTest/Pages/RecipeCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeTest.Pages
+{
+    public class RecipeCard
+    {
+        public string RecipeName { get; set; }
+        public string CoverPhoto { get; set; }
+        public string RecipeIntro { get; set; }
+        public string CookingTime { get; set; }
+        public decimal Portion { get; set; }
+        public decimal Rating { get; set; }
+    }
+}

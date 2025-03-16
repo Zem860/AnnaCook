db06d768aebb58253367620648ecc0fdd53c1854
Zem860 <zemmyhammy@gmail.com>
2025-03-16 12:54:34 +0800

    integrate recipe object to RecipeRelated

---
+++ RecipeTest/Pages/RecipeRelated.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RecipeTest.Models;
+
+namespace RecipeTest.Pages
+{
+    public class RecipeRelated
+    {
+        public class RecipeCard
+        {
+            public int RecipeId { get; set; }
+            public string RecipeName { get; set; }
+            public string CoverPhoto { get; set; }
+            public string RecipeIntro { get; set; }
+            public string CookingTime { get; set; }
+            public decimal Portion { get; set; }
+            public decimal Rating { get; set; }
+        }
+
+        //Post使用創建食譜需要
+        public class RecipePlaceholder
+        {
+            public string RecipeName { get; set; }
+            public string CoverPhoto { get; set; }
+        }
+
+   
+    }
+}

8e4ac74953e88644b95d4026a51220716233538c
Zem860 <zemmyhammy@gmail.com>
2025-04-13 23:11:18 +0800

    update: record login status to login

---
+++ RecipeTest/Enums/LoginActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeTest.Enums
+{
+    public enum LoginActions
+    {
+        LoginSuccess =0,
+        LoginFail = 1,
+        ChangePassword = 2,
+
+    }
+}
---
+++ RecipeTest/Models/LoginRecords.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RecipeTest.Enums;
+namespace RecipeTest.Models
+{
+    public class LoginRecords
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "編號" )]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "會員編號")]
+        public int UserId { get; set; }
+
+
+        [MaxLength(45)]
+        [Column(TypeName = "nvarchar")]
+        [Display(Name = "IP 位址")]
+        public string IpAddress { get; set; } = null;
+
+
+        [Column(TypeName = "int")]
+        [Display(Name = "登入狀態")]
+        public int LoginAction { get; set; } = (int)LoginActions.LoginSuccess;  
+
+
+        [Display(Name = "動作時間")]
+        [Column(TypeName = "DATETIME")]
+          
+        public DateTime ActionTime { get; set; } = DateTime.Now;
+
+    }
+}

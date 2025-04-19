2ae09f23bc194e89a3f112224739c585aa38ce4f
Zem860 <zemmyhammy@gmail.com>
2025-04-19 09:49:15 +0800

    feat: complete addViewLog table and AdDisplayPage Enum

---
+++ RecipeTest/Enums/AdDisplayPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RecipeTest.Enums
+{
+    public enum AdDisplayPageType
+    {
+        [Display(Name = "未知")]
+        Unknown = 0,
+
+        [Display(Name = "首頁廣告")]
+        Home = 1,
+        [Display(Name = "食譜列表頁廣告")]
+        SearchList = 2,
+
+        [Display(Name = "食譜內頁廣告")]
+        RecipeDetail = 3,
+
+    }
+
+}
---
+++ RecipeTest/Models/AdViewLog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeTest.Models
+{
+    public class AdViewLog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name ="編號")]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "int")]
+        [Display(Name ="廣告id")]
+        public int AdId { get; set; }
+
+        [Column(TypeName ="int")]
+        [Display(Name ="使用者Id")]
+        public int? UserId { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [Display(Name ="SessionId")]
+        [MaxLength(100)]
+        public string SessionId { get; set; }
+
+        [Column(TypeName ="bit")]
+        [Display(Name ="點擊")]
+        public bool IsClick {  get; set; }     // true = 點擊，false = 曝光
+
+        [Display(Name = "曝光頁面")]
+        public int AdDisplayPage { get; set; } = 0; // 建議加這個欄位
+
+        [Column(TypeName ="DATETIME")]
+        [Display(Name ="觸發時間")]
+        public DateTime ViewedAt { get; set; }
+
+    }
+}

72599e2de57a4cd33d5f4ca70652a4e85eb3b099
Zem860 <zemmyhammy@gmail.com>
2025-04-20 22:11:19 +0800

    fix: change ways to get token data in Okcontroller

--- RecipeTest/Controllers/OkController.cs
+++ RecipeTest/Controllers/OkController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using MyWebApiProject.Security;
 using RecipeTest.Enums;
@@ -20,7 +21,7 @@ namespace RecipeTest.Controllers
         public IHttpActionResult SayOk()
         {
             string ip = userhash.getClientIp();
-            return Ok(new { message = "Ok", ip=ip });
+            return Ok(new { message = "Ok", ip = ip });
         }
 
         [HttpGet]
@@ -28,16 +29,16 @@ namespace RecipeTest.Controllers
         [JwtAuthFilter]
         public IHttpActionResult Check()
         {
-            var payload = (Dictionary<string, object>)Request.Properties["JwtPayload"];
+            var payload = userhash.GetUserFromJWT();
             var user = new UserRelated.UserTokenData
             {
-                Id = (int)payload["Id"],
-                DisplayId = payload["DisplayId"].ToString(),
-                AccountEmail = payload["AccountEmail"].ToString(),
-                AccountName = payload["AccountName"].ToString(),
-                ProfilePhoto = payload.ContainsKey("ProfilePhoto") ? payload["ProfilePhoto"].ToString() : null,
-                Role = Convert.ToInt32(payload["Role"]),
-                LoginProvider = (int)(LoginProvider)Convert.ToInt32(payload["LoginProvider"])
+                Id = (int)payload.Id,
+                DisplayId = payload.DisplayId,
+                AccountEmail = payload.AccountEmail,
+                AccountName =payload.AccountName,
+                ProfilePhoto = payload.ProfilePhoto,
+                Role = Convert.ToInt32(payload.Role),
+                LoginProvider = (int)(LoginProvider)Convert.ToInt32(payload.LoginProvider)
             };
 
             // ✅ 重新產生新 token
@@ -50,6 +51,5 @@ namespace RecipeTest.Controllers
                 token = newToken
             });
         }
-
     }
 }

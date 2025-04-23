c8f373b26a11c4e428cca3c2b36b7867519cb86b
Zem860 <zemmyhammy@gmail.com>
2025-04-23 09:50:34 +0800

    feat: add checkVimeoStatusChecker

--- RecipeTest/Controllers/OkController.cs
+++ RecipeTest/Controllers/OkController.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using MyWebApiProject.Security;
+using Newtonsoft.Json;
 using RecipeTest.Enums;
+using RecipeTest.Models;
 using RecipeTest.Pages;
 using RecipeTest.Security;
 
@@ -15,6 +21,70 @@ namespace RecipeTest.Controllers
     public class OkController : ApiController
     {
         private UserEncryption userhash = new UserEncryption();
+        private string vimeoAccessToken = ConfigurationManager.AppSettings["VimeoAccessToken"];
+        private RecipeModel db = new RecipeModel();
+
+        public class VimeoStatusResult
+        {
+            public UploadInfo upload { get; set; }
+            public TranscodeInfo transcode { get; set; }
+
+            public class UploadInfo
+            {
+                public string status { get; set; }
+            }
+
+            public class TranscodeInfo
+            {
+                public string status { get; set; }
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/checkvimeo")]
+        [JwtAuthFilter]
+        public async Task<IHttpActionResult> CheckAPI(string videoId)
+        {
+            var userhash = new UserEncryption();
+            var user = userhash.GetUserFromJWT();
+            var userData = db.Users.FirstOrDefault(u => u.Id == user.Id && !u.IsBanned && !u.IsDeleted && u.IsVerified && u.UserRole == UserRoles.User);
+            bool hasUser = userData != null;
+            if (!hasUser)
+            {
+                return Ok(new { StatusCode = 403, msg = "你沒有權限" });
+            }
+            string requestUrl = $"https://api.vimeo.com/videos/{videoId}?fields=uri,upload.status,transcode.status";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", vimeoAccessToken);
+
+                try
+                {
+                    var response = await client.GetAsync(requestUrl);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return Ok(new { });
+                    }
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<VimeoStatusResult>(content);
+
+                    bool isWatchable = result.transcode?.status == "in_progress" ? false : true;
+                    return Ok(new
+                    {   StatusCode = 200,
+                        msg = "獲取影片進度",
+                        isWatchable = isWatchable
+                    });
+                }
+                catch (Exception ex)
+                {
+                    return InternalServerError(ex);
+                }
+            }
+        }
+
 
         [HttpGet]
         [Route("api/Ok")]
